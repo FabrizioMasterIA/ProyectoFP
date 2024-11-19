f2cdcc5cf9501d8eb9a0d068d0122d76cc1e1427
Heriberto Diaz <24690705@tecvalles.mx>
2024-11-19 00:04:12 -0600

    Se modifico el programa principal

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
-from src.modulo import generar_numeros_aleatorios, pedir_numeros_usuario, comparar_resultados
+from src.modulo import generar_numeros_aleatorios, comparar_resultados
+
+def pedir_numeros_usuario():
+    while True:
+        try:
+            # Pedir al usuario que ingrese 5 números separados por comas
+            entrada = input("Por favor, ingresa 5 números separados por comas: ")
+            numeros_usuario = [int(num.strip()) for num in entrada.split(",")]
+            
+            # Verificar que se ingresen exactamente 5 números
+            if len(numeros_usuario) != 5:
+                print("Debes ingresar exactamente 5 números.")
+                continue
+            
+            return numeros_usuario
+        except ValueError:
+            print("Entrada no válida. Asegúrate de ingresar solo números enteros separados por comas.")
 
 def main():
     print("¡Bienvenido al sorteo de la lotería!")
     
+    # Pedir los números al usuario
+    numeros_usuario = pedir_numeros_usuario()
+    
     # Generar los 5 números sorteados aleatoriamente
     numeros_sorteo = generar_numeros_aleatorios()
     print("Los números sorteados son:", numeros_sorteo)
-    
-    # Pedir los números al usuario
-    numeros_usuario = pedir_numeros_usuario()
     
     # Comparar los números del usuario con los sorteados
     acertados, numeros_acertados = comparar_resultados(numeros_usuario, numeros_sorteo)
